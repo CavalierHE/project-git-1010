0db6274313a9fe375582ee932def6bc7a0d9d44b
whyihandsome <whyihandsome@163.com>
2020-03-15 21:12:12 +0800

    test branch

--- src/main/java/com/atguigu/git/Apple.java
+++ src/main/java/com/atguigu/git/Apple.java
@@ -5,5 +5,6 @@ public class Apple {
         System.out.println("hello world");
         System.out.println("hello git");
         System.out.println("hot fix......");
+        System.out.println("hot master......");
     }
 }
