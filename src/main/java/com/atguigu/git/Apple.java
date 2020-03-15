b75b1502add4bc0a6122763cca76db24d82b26cf
whyihandsome <whyihandsome@163.com>
2020-03-15 20:15:36 +0800

    test git branch and github

--- src/main/java/com/atguigu/git/Apple.java
+++ src/main/java/com/atguigu/git/Apple.java
@@ -4,5 +4,6 @@ public class Apple {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello git");
+        System.out.println("hot fix......");
     }
 }
